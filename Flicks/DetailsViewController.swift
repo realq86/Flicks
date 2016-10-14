0060f19808ca1edd290d27ed6a21658f550058cd
Michael <realq86@gmail.com>
2016-10-13 22:49:23 -0700

    Create DetailVC with ImageView as background.  ScrollView with a nested Label as the foreground.

---
+++ Flicks/DetailsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailsViewController.swift
+//  Flicks
+//
+//  Created by Developer on 10/13/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+class DetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    var backgroundImageURL:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.backgroundImageView.setImageWith(URL(string: self.backgroundImageURL!)!)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        
+        print("BUTTON PRESSED!!!")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
