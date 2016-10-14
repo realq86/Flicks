b408d32a8995122ff102ae377fb11fcfbf6ca043
Michael <realq86@gmail.com>
2016-10-13 17:53:50 -0700

    New Custom TableViewCell Class with: Movie ImageView Movie Title Label Movie Overview Label.
    Autolayout set in storyboard

---
+++ Flicks/MovieCell/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Developer on 10/13/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieOverview: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
