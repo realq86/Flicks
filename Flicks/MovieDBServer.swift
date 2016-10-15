e7384a57edf426f722fb7e3f6917d27f93e4f70b
Michael <realq86@gmail.com>
2016-10-14 21:32:29 -0700

    Creating api call to topRated end point.

--- Flicks/MovieDBServer.swift
+++ Flicks/MovieDBServer.swift
@@ -12,6 +12,7 @@ import AFNetworking
 private let movieDBAPIKey = ["api_key":"a07e22bc18f5cb106bfe4cc1f83ad8ed"]
 private let baseURL = "https://api.themoviedb.org/3/movie"
 private let nowPlayingEndPoint = "/now_playing"
+private let topRatedEndPoint = "/top_rated"
 
 class MovieDBServer: NSObject {
 
@@ -38,6 +39,37 @@ class MovieDBServer: NSObject {
             },
                     success: { (task:URLSessionDataTask, response:Any?) in
                                                 
+                        if let response = response as? Dictionary<String, AnyObject> {
+                            
+                            self.nowPlayingResponse = response
+                            self.nowPlayingArray = self.nowPlayingResponse["results"] as! [AnyObject]
+                            print("Array     \(self.nowPlayingArray)")
+                        }
+                        
+                        responseBlock(self.nowPlayingArray, nil)
+                        
+            },
+                    failure:{ (task:URLSessionDataTask?, error:Error) in
+                        print(error.localizedDescription)
+                        responseBlock(self.nowPlayingArray, error)
+        })
+        
+    }
+    
+    func getTopRated(_ responseBlock: @escaping (Array<AnyObject>, Error?)->()) {
+        
+        let nowPlayingURL = "\(baseURL)\(topRatedEndPoint)"
+        
+        let manager = AFHTTPSessionManager()
+        
+        
+        manager.get(nowPlayingURL,
+                    parameters: movieDBAPIKey,
+                    progress: { (progress:Progress) in
+                        
+            },
+                    success: { (task:URLSessionDataTask, response:Any?) in
+                        
                         if let response = response as? Dictionary<String, AnyObject> {
                             
                             self.nowPlayingResponse = response
