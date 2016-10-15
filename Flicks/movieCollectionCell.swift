3f5b8a9aee607b59d1c9fab4b4383f0e1e884c29
Michael <realq86@gmail.com>
2016-10-15 15:56:05 -0700

    New collectionViewCell

---
+++ Flicks/movieCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  movieCollectionCell.swift
+//  Flicks
+//
+//  Created by Developer on 10/15/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class movieCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+}
